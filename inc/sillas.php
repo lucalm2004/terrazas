f7b936acf0afdc18c35073f55b0102be7d914376
luca <lucalm2004@gmail.com>
2024-01-18 16:38:43 +0100

    gestionSillas

---
+++ inc/sillas.php
@@ -0,0 +1,57 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['id'])) {
+    header("Location: ./index.php");
+    exit;
+} elseif (isset($_GET['logout'])) {
+    session_destroy();
+    header("Location: ./index.php");
+    exit;
+}
+require_once './conexion.php'; // Asegúrate de que la ruta sea correcta
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $id_mesa = $_POST['id_mesa'];
+    $id_sala = $_POST['id_sala'];
+    $sillas = $_POST['sillas'] + 1;
+    $silla = $_POST['sillas'] - 1;
+
+
+
+    try {
+        // Verificar qué botón se presionó
+        if (isset($_POST['add_silla'])) {
+            $sql = "UPDATE mesas SET sillas = :sillas WHERE id_mesa = :id_mesa";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id_mesa', $id_mesa, PDO::PARAM_INT);
+            $stmt->bindParam(':sillas', $sillas, PDO::PARAM_INT);
+        } elseif (isset($_POST['remove_silla'])) {
+            $sql = "UPDATE mesas SET sillas = :sillas WHERE id_mesa = :id_mesa";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':id_mesa', $id_mesa, PDO::PARAM_INT);
+            $stmt->bindParam(':sillas', $silla, PDO::PARAM_INT);
+        }
+        // Preparar la consulta
+       
+
+        // Ejecutar la consulta
+        if ($stmt->execute()) {
+            if ($id_sala >= 1 && $id_sala <= 3) {
+                header("Location: ../mesas.php?id=" . $id_sala);
+            } elseif ($id_sala >= 4 && $id_sala <= 5) {
+                header("Location: ../mesas.php?id=" . $id_sala);
+            } elseif ($id_sala >= 6 && $id_sala <= 9) {
+                header("Location: ../mostrar.php?id=Privada");
+            } else {
+                echo "Error: ID de sala desconocido.";
+            }
+            die();
+                } else {
+                    echo "Error en la operación";
+                }
+    } catch (PDOException $e) {
+        echo "Error en la conexión a la base de datos: " . $e->getMessage();
+    }
+}
+?>
--- mesas.php
+++ mesas.php
@@ -12,6 +12,7 @@ if (!isset($_SESSION['id'])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -31,20 +32,20 @@ if (!isset($_SESSION['id'])) {
 <body>
     <nav class="navbar navbar-light bg-lights position-top">
         <div class="container">
-        <div>
-            <a class="navbar-brand " href="./home.php">
-                <img src="./img/LOGORICK _Blanco.png" alt="" width="100" height="90">
-                <a href="./registro.php"><button class="atrasboton"><img class="atrasimg" src="./img/libro.png" alt=""></button></a>
+            <div>
+                <a class="navbar-brand " href="./home.php">
+                    <img src="./img/LOGORICK _Blanco.png" alt="" width="100" height="90">
+                    <a href="./registro.php"><button class="atrasboton"><img class="atrasimg" src="./img/libro.png" alt=""></button></a>
 
-            </a>
+                </a>
             </div>
             <div class="saludo">
-            <b>¡Bienvenido al portal, <?php echo $_SESSION['user'];?>!</b>
+                <b>¡Bienvenido al portal, <?php echo $_SESSION['user']; ?>!</b>
             </div>
             <div>
-            
-            <a href="./home.php"><button class="atrasboton"><img class="atrasimg" src="./img/atras.png" alt=""></button></a>
-            <a href="./inc/salir.php"><button class="logoutboton"><img class="logoutimg" src="./img/LOGOUT.png" alt=""></button></a>
+
+                <a href="./home.php"><button class="atrasboton"><img class="atrasimg" src="./img/atras.png" alt=""></button></a>
+                <a href="./inc/salir.php"><button class="logoutboton"><img class="logoutimg" src="./img/LOGOUT.png" alt=""></button></a>
             </div>
         </div>
     </nav>
@@ -56,18 +57,17 @@ if (!isset($_SESSION['id'])) {
     } else {
         try {
             require './inc/conexion.php';
-        
+
             $id = trim($_GET['id']);
             $sql = "SELECT * FROM mesas WHERE id_sala = :id";
             $stmt = $conn->prepare($sql);
             $stmt->bindParam(':id', $id, PDO::PARAM_STR);
             $stmt->execute();
             $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
-        
         } catch (PDOException $e) {
             echo "Error: " . $e->getMessage();
         }
-        
+
         echo '<div class="centrado">';
         $i = 0;
         foreach ($res as $mesa) {
@@ -76,79 +76,91 @@ if (!isset($_SESSION['id'])) {
             }
             echo '<a><div class="image-item">';
             if ($mesa['estado'] == "ocupada" || $mesa['estado'] == "reservado") {
-                if($mesa['estado'] == "mantenimiento"){
+                if ($mesa['estado'] == "mantenimiento") {
                     echo '<img class="filtro2" src="./img/mesas.png" alt="Imagen 1">';
-                }else{
+                } else {
                     echo '<img class="filtro" src="./img/mesas.png" alt="Imagen 1">';
-
                 }
-                echo '<div class="image-text"><h2> Mesa'.$mesa['numero_mesa'].'</h2>';
-                echo '<p class="diss">'.$mesa['estado'].'</p>';
+                echo '<div class="image-text"><h2> Mesa' . $mesa['numero_mesa'] . '</h2>';
+                echo '<p class="diss">' . $mesa['estado'] . '</p>';
                 $clase = 'class ="btn2 danger  btn-block" value="Desocupar" ';
             } else {
-                if($mesa['estado'] == "mantenimiento"){
+                if ($mesa['estado'] == "mantenimiento") {
                     echo '<img class="filtro2" src="./img/mesas.png" alt="Imagen 1">';
-                }else{
+                } else {
                     echo '<img class="" src="./img/mesas.png" alt="Imagen 1">';
-
                 }
-                echo '<div class="image-text"><h2> Mesa'.$mesa['numero_mesa'].'</h2>';
-                echo '<p><b>Estado: </b>'.$mesa['estado'].'</p>';
-                if($_SESSION['user'] == 'mantenimiento'){
-                    echo '<b>Sillas: </b><input style="margin-bottom:5%" type="text" value="'.$mesa['sillas'].'"></input>';
-                    if($mesa['estado'] == 'mantenimiento'){
+                echo '<div class="image-text"><h2> Mesa' . $mesa['numero_mesa'] . '</h2>';
+                echo '<p><b>Estado: </b>' . $mesa['estado'] . '</p>';
+                if ($_SESSION['user'] == 'mantenimiento') {
+                    echo '<form action="sillas.php?id=' . $mesa['numero_mesa'] . '">';
+                    echo '<b>Sillas: </b><input style="margin-bottom:5%" type="text" value="' . $mesa['sillas'] . '"></input>';
+                    echo '</form>';
+                    if ($mesa['estado'] == 'mantenimiento') {
                         $clase = 'class ="btn2 success btn-block" value="Arreglado/Mantenimiento" ';
-                    }else{
+                    } else {
                         $clase = 'class ="btn2 danger btn-block" value="Mantenimiento/Arreglado" ';
-
                     }
-                }elseif($mesa['estado'] !== 'mantenimiento'){
-                    echo '<p><b>Sillas: </b>'.$mesa['sillas'].'</p>';
+                } elseif ($mesa['estado'] !== 'mantenimiento') {
+                    echo '<form method="POST" action="./inc/sillas.php?id=' . $mesa['numero_mesa'] . '">';
+                    echo "<input type='hidden' name='id_sala' value=" . $mesa['id_sala'] . ">";
+                    echo "<input type='hidden' name='id_mesa' value=" . $mesa['id_mesa'] . ">";
+
+                    echo "<input type='hidden' name='sillas' value=" . $mesa['sillas'] . ">";
+                    echo '<div class="d-flex align-items-center justify-content-between">';
+                    echo '<p><b>Sillas: </b>' . $mesa['sillas'] . '</p>';
+                    echo '<div>';
+                    echo '<span>';
+                    echo '<button style="margin-bottom: 30%;" type="submit" name="add_silla" class="btn btn-success">+</button>';
+                    echo '  ';
+                    echo '<button style="margin-bottom: 30%" type="submit" name="remove_silla" class="btn btn-danger">-</button>';
+                    echo '</span>';
+                    echo '</div>';
+
+                    echo '</div>';
+                    echo '</form>';
+
                     $clase = 'class ="btn2 success btn-block" value="Ocupar" ';
-                }else{
+                } else {
                     $clase = 'class ="btn btn-secondary btn-block" value="Mantenimiento" disabled';
                 }
-                
             }
-            
-            if($mesa['estado'] == "reservado"){
+
+            if ($mesa['estado'] == "reservado") {
                 echo "<form action='./inc/procesar_reserva.php' method='post'>";
-                echo "<input type='hidden' name='id_sala' value=".$mesa['id_sala'].">";
-            echo "<input type='hidden' name='id_mesa' value=".$mesa['id_mesa'].">";
-            echo "<input type='hidden' name='numero_mesa' value=".$mesa['numero_mesa'].">";
-            echo '<p> <b>Nombre de la reserva: </b>'.$mesa['nombre_reserva'].'</p>';
-            echo '<p><b>Hora de la Reserva: </b>'.$mesa['hora_reserva'].'</p>';
-            echo '<p><b>Hora fin Reserva: </b>'.$mesa['hora_fin_reserva'].'</p>';
-            echo '<p><b>Dia de la reserva: </b>'.$mesa['dia_reserva'].'</p>';
+                echo "<input type='hidden' name='id_sala' value=" . $mesa['id_sala'] . ">";
+                echo "<input type='hidden' name='id_mesa' value=" . $mesa['id_mesa'] . ">";
+                echo "<input type='hidden' name='numero_mesa' value=" . $mesa['numero_mesa'] . ">";
+                echo '<p> <b>Nombre de la reserva: </b>' . $mesa['nombre_reserva'] . '</p>';
+                echo '<p><b>Hora de la Reserva: </b>' . $mesa['hora_reserva'] . '</p>';
+                echo '<p><b>Hora fin Reserva: </b>' . $mesa['hora_fin_reserva'] . '</p>';
+                echo '<p><b>Dia de la reserva: </b>' . $mesa['dia_reserva'] . '</p>';
 
-            echo "<input type='hidden' name='desreserva' value='desreserva'>";
-            }else{
+                echo "<input type='hidden' name='desreserva' value='desreserva'>";
+            } else {
                 echo "<form method='POST' action='./inc/procesar.php'>";
-                echo "<input type='hidden' name='id_sala' value=".$mesa['id_sala'].">";
-                echo "<input type='hidden' name='id_mesa' value=".$mesa['id_mesa'].">";
-                echo "<input type='hidden' name='numero_mesa' value=".$mesa['numero_mesa'].">";
+                
+                echo "<input type='hidden' name='id_sala' value=" . $mesa['id_sala'] . ">";
+                echo "<input type='hidden' name='id_mesa' value=" . $mesa['id_mesa'] . ">";
+                echo "<input type='hidden' name='numero_mesa' value=" . $mesa['numero_mesa'] . ">";
             }
-            
-            
-            if($mesa['estado'] == "reservado" && $_SESSION['user'] !== 'mantenimiento' ){
-               if($mesa['estado'] !== "mantenimiento"){
-                echo "<input class='btn2 danger btn-block' value='Desreservar' type='submit'>";
 
-               }
-            }elseif($_SESSION['user'] !== 'mantenimiento' ){
-                echo "<input ".$clase." type='submit'>";
-                if($mesa['estado'] !== "mantenimiento"){
+
+            if ($mesa['estado'] == "reservado" && $_SESSION['user'] !== 'mantenimiento') {
+                if ($mesa['estado'] !== "mantenimiento") {
+                    echo "<input class='btn2 danger btn-block' value='Desreservar' type='submit'>";
+                }
+            } elseif ($_SESSION['user'] !== 'mantenimiento') {
+                echo "<input " . $clase . " type='submit'>";
+                if ($mesa['estado'] !== "mantenimiento") {
                     echo "<input class = 'btn2 danger btn-block' name='estadoSilla' value='Mantenimiento' type='submit'>";
-
                 }
 
-                if($mesa['estado'] !== "mantenimiento"){
+                if ($mesa['estado'] !== "mantenimiento") {
                     echo "<input class='btn2 success btn-block' value='Reservar' type='button' onclick='mostrarReservaModal(\"{$mesa['id_mesa']}\", \"{$mesa['numero_mesa']}\")'>";
-    
-                   }
-            }else{
-                echo "<input ".$clase." name='estadoSilla' type='submit'>";
-
+                }
+            } else {
+                echo "<input " . $clase . " name='estadoSilla' type='submit'>";
             }
             echo "</form>";
             echo '</div></div></a>';
@@ -163,58 +175,59 @@ if (!isset($_SESSION['id'])) {
 
     <!-- Modal de Reserva -->
     <div class="modal fade" id="reservaModal" tabindex="-1" aria-labelledby="reservaModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="reservaModalLabel">Realizar Reserva</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-                <!-- Agrega un elemento para mostrar el número/nombre de la mesa -->
-                <div id="infoMesa" class="mb-3"></div>
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="reservaModalLabel">Realizar Reserva</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <!-- Agrega un elemento para mostrar el número/nombre de la mesa -->
+                    <div id="infoMesa" class="mb-3"></div>
 
-                <!-- Formulario de reserva -->
-                <form id="reservaForm" method="post" action="./inc/procesar_reserva.php" onsubmit="return validarReserva()">
-                    <!-- Otros campos del formulario -->
+                    <!-- Formulario de reserva -->
+                    <form id="reservaForm" method="post" action="./inc/procesar_reserva.php" onsubmit="return validarReserva()">
+                        <!-- Otros campos del formulario -->
 
-                    <!-- Campo oculto para almacenar el id de la mesa -->
-                    <input type="hidden" id="id_mesa_reserva" name="id_mesa_reserva">
+                        <!-- Campo oculto para almacenar el id de la mesa -->
+                        <input type="hidden" id="id_mesa_reserva" name="id_mesa_reserva">
 
-                    <!-- Campo oculto para el estado de la mesa -->
-                    <input type="hidden" name="estado_mesa" value="reservado">
+                        <!-- Campo oculto para el estado de la mesa -->
+                        <input type="hidden" name="estado_mesa" value="reservado">
 
-                    <!-- Nombre -->
-                    <label for="nombre_reserva">Nombre:</label>
-                    <input type="text" id="nombre_reserva" name="nombre_reserva" oninput="validarNombre()" required>
-                    <p id="nombreError" style="color: red;"></p>
+                        <!-- Nombre -->
+                        <label for="nombre_reserva">Nombre:</label>
+                        <input type="text" id="nombre_reserva" name="nombre_reserva" oninput="validarNombre()" required>
+                        <p id="nombreError" style="color: red;"></p>
 
-                    <!-- Hora -->
-                    <label for="hora_reserva">Hora:</label>
-                    <input type="time" id="hora_reserva" name="hora_reserva" oninput="validarHora()" required>
-                    <p id="horaError" style="color: red;"></p>
+                        <!-- Hora -->
+                        <label for="hora_reserva">Hora:</label>
+                        <input type="time" id="hora_reserva" name="hora_reserva" oninput="validarHora()" required>
+                        <p id="horaError" style="color: red;"></p>
 
-                    <!-- Hora de fin -->
-                    <label for="hora_fin_reserva">Hora de fin:</label>
-                    <input type="time" id="hora_fin_reserva" name="hora_fin_reserva" oninput="validarHoraFin()" required>
-                    <p id="errorFechaFin" style="color: red;"></p>
+                        <!-- Hora de fin -->
+                        <label for="hora_fin_reserva">Hora de fin:</label>
+                        <input type="time" id="hora_fin_reserva" name="hora_fin_reserva" oninput="validarHoraFin()" required>
+                        <p id="errorFechaFin" style="color: red;"></p>
 
-                    <!-- Día -->
-                    <label for="dia_reserva">Día:</label>
-                    <input type="date" id="dia_reserva" name="dia_reserva" oninput="validarDia()" required>
-                    <p id="diaError" style="color: red;"></p>
+                        <!-- Día -->
+                        <label for="dia_reserva">Día:</label>
+                        <input type="date" id="dia_reserva" name="dia_reserva" oninput="validarDia()" required>
+                        <p id="diaError" style="color: red;"></p>
 
-                    <!-- ID Sala -->
-                    <input type="hidden" name="id_sala" value="<?php echo isset($_GET['id']) ? htmlspecialchars($_GET['id']) : ''; ?>">
+                        <!-- ID Sala -->
+                        <input type="hidden" name="id_sala" value="<?php echo isset($_GET['id']) ? htmlspecialchars($_GET['id']) : ''; ?>">
 
-                    <button type="submit" class="btn btn-primary">Enviar Reserva</button>
-                </form>
+                        <button type="submit" class="btn btn-primary">Enviar Reserva</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-</div>
-<script src="./js/mesas.js"></script>
+    <script src="./js/mesas.js"></script>
 
 
 
 </body>
+
 </html>
