ba794efbce07581c468f83be36b0c4fcfe0e3022
luca <lucalm2004@gmail.com>
2024-01-24 17:03:21 +0100

    versionBuena

--- inc/validaciones.php
+++ inc/validaciones.php
@@ -1,13 +1,13 @@
 <?php
-session_start();
-if (!isset($_SESSION['id'])) {
-    header("Location: ./index.php");
-    exit;
-} else if (isset($_GET['logout'])) {
-    session_destroy();
-    header("Location: ./index.php");
-    exit;
-}
+// session_start();
+// if (!isset($_SESSION['id'])) {
+//     header("Location: ./index.php");
+//     exit;
+// } else if (isset($_GET['logout'])) {
+//     session_destroy();
+//     header("Location: ./index.php");
+//     exit;
+// }
 
 
 if (!filter_has_var(INPUT_POST, 'inicio')) {
